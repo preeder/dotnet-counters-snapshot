62d5ae064322a646246525ed1e685962e5966613
Paul Reeder <paul.reeder@solfa.co>
2020-05-02 03:04:27 -0500

    Initial checkin

---
+++ Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Diagnostics.NETCore.Client;
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
+
+namespace CounterSnapshot
+{
+	class Program
+	{
+		static void ShowHelp()
+		{
+			Console.WriteLine("Usage:");
+			Console.WriteLine("  dotnet-counters-snapshot [options] [command]");
+			Console.WriteLine();
+			Console.WriteLine("Options:");
+			Console.WriteLine("  --version    Display version information");
+			Console.WriteLine();
+			Console.WriteLine("Commands:");
+			Console.WriteLine("  help");
+			Console.WriteLine("    This help");
+			Console.WriteLine("  snapshot (-p|--process-id) <pid> [(-m|--max-time) <period_in_ms>] <counter_list[]>");
+			Console.WriteLine("    Collects a snapshot of each counter in the counter_list, then exits");
+			Console.WriteLine();
+		}
+
+		static void Main(string[] args)
+		{
+			if (args.Length == 0 || args[0] == "help" || (args[0] != "--version" && args[0] != "snapshot"))
+			{
+				ShowHelp();
+				return;
+			}
+			if (args[0] == "--version")
+			{
+				Console.WriteLine(Assembly.GetCallingAssembly().GetName().Version.ToString());
+				Console.WriteLine();
+				return;
+			}
+
+			//parse args 
+			var countersToCollect = new Dictionary<string, List<string>>();
+			var inCounters = false;
+			int? pid = null;
+			var maxTime = TimeSpan.FromMilliseconds(1000);
+			for (var i = 1; i < args.Length; ++i)
+			{
+				if (!inCounters)
+				{
+					switch (args[i])
+					{
+						case "-p":
+						case "--process-id":
+							pid = int.Parse(args[++i], CultureInfo.CurrentCulture);
+							continue;
+						case "-m":
+						case "--max-time":
+							maxTime = TimeSpan.FromMilliseconds(double.Parse(args[++i], CultureInfo.CurrentCulture));
+							continue;
+						default:
+							inCounters = true;
+							break;
+					}
+				}
+
+				if (!Regex.IsMatch(args[i], @"^\w\S+\w\[[\w\-]+(,[\w\-]+)*\]$"))
+				{
+					ShowHelp();
+					return;
+				}
+
+				var delimiter = args[i].IndexOf('[', StringComparison.CurrentCulture);
+				var provider = args[i].Substring(0, delimiter);
+				var counters = args[i].Substring(delimiter + 1, args[i].Length - delimiter - 2).Split(',');
+				if (countersToCollect.ContainsKey(provider))
+				{
+					countersToCollect[provider].AddRange(counters);
+				}
+				else
+				{
+					countersToCollect.Add(provider, new List<string>(counters));
+				}
+			}
+
+			if (!pid.HasValue || !countersToCollect.Any())
+			{
+                ShowHelp();
+				return;
+			}
+
+			//now collect the counters
+			var pipelineArguments = new Dictionary<string, string> {{"EventCounterIntervalSec", "1"}};
+			var providers = countersToCollect.Select(z => new EventPipeProvider(z.Key, EventLevel.Informational, (long) ClrTraceEventParser.Keywords.None, pipelineArguments));
+			var client = new DiagnosticsClient(pid.Value);
+			using (var session = client.StartEventPipeSession(providers, false))
+			{
+				var cancellationSource = new CancellationTokenSource();
+                var getCounters = Task.Run(() =>
+				{
+					var source = new EventPipeEventSource(session.EventStream);
+					source.Dynamic.All += (traceEvent =>
+					{
+						if (traceEvent.EventName == "EventCounters")
+						{
+							var provider = traceEvent.ProviderName;
+							var metricInfo = ((traceEvent.PayloadValue(0) as IDictionary<string, object>)["Payload"] as IDictionary<string, object>);
+							var metricName = metricInfo["Name"].ToString();
+							var metricValue = double.Parse((metricInfo["Mean"] ?? metricInfo["Increment"]).ToString());
+							lock (countersToCollect)
+							{
+								if (countersToCollect.ContainsKey(provider) && countersToCollect[provider].Contains(metricName))
+								{
+									Console.WriteLine($"{provider}/{metricName} {metricValue}");
+									if (countersToCollect[provider].Count == 1)
+									{
+										if (countersToCollect.Count == 1)
+										{
+											//NOTE: we have to do this, else we end up getting the last counter multiple times
+											countersToCollect.Clear();
+											cancellationSource.Cancel();
+										}
+										else
+										{
+											countersToCollect.Remove(provider);
+										}
+									}
+									else
+									{
+										countersToCollect[provider].Remove(metricName);
+									}
+								}
+							}
+						}
+					});
+					source.Process();
+				}, cancellationSource.Token);
+				var waitTask = Task.Delay(maxTime, cancellationSource.Token);
+				Task.WaitAny(getCounters, waitTask);
+				session.Stop();
+			}
+		}
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
+﻿
 # dotnet-counters-snapshot
-Tool to take a snapshot of metric data a la dotnet-counters
+
+This tool fills the void in the very nice dotnet core 3.1 tool dotnet-counters by supplying the capability to gather a single snapshot of data from a set of counters.
+
+This can then be used in a text pipeline to provide additional functionality.  For example, we use this bash script (fired each minute from cron) to update aws cloudwatch counters.
+
+
+```bash
+#!/bin/bash
+# collects metrics for InventoryWeb service and sends to Cloudwatch<br />
+<br />
+# get PID
+PID=`ps -ax | grep InventoryWeb | grep -v grep | sed -e 's/^ *//' -e 's/ .*//'`
+
+# collect metrics
+dotnet-counters-snapshot snapshot -p $PID -m 10000 System.Runtime[cpu-usage,working-set] \
+   InventoryImport.Metrics[queued,processed,errors,queue-time,processing-time,total-time,slots-available] \
+   | sed -e 's/System.Runtime\//VehicleInventory\/System /' -e 's/InventoryImport.Metrics\//VehicleInventory\/Imports /' \
+   | while IFS= read line; do
+        #split it
+        atoms=($(echo "$line"))
+        name=${atoms[0]}
+        metric=${atoms[1]}
+        value=${atoms[2]}
+
+        aws cloudwatch put-metric-data --namespace $name --metric-name=$metric --value $value
+        echo `date "+%Y%m%d %H%M%S"` $name::$metric $value >> /opt/vehicleInventory/sendMetrics.log
+done
+```
